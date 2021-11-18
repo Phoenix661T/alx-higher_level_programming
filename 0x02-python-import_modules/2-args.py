a494758dc33190959bea56373ce4850c908cda48
Phoenix661T <thabangmaloka661@outlook.com>
2021-11-17 22:13:46 -0800

    bug fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,13 +4,13 @@ from sys import argv
 
 def principal():
         print('{} argument'.format(len(argv) - 1), end='')
-            if len(argv) == 1:
-                        print('s.')
-            elif len(argv) == 2:
-                        print(':')
+        if len(argv) == 1:
+                print('s.')
+        elif len(argv) == 2:
+                print(':')
             else:
-                        print('s:')
-                            for i in range(1, len(argv)):
-                                        print('{}: {}'.format(i, argv[i]))
-                                        if __name__ == "__main__":
-                                                principal()
+                print('s:')
+        for i in range(1, len(argv)):
+                print('{}: {}'.format(i, argv[i]))
+        if __name__ == "__main__":
+           principal()
