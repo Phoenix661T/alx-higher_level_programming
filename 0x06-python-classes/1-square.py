953e0c61c91d61d285e0a1bb233c180c0665df3b
Phoenix661T <thabangmaloka661@outlook.com>
2021-12-07 09:12:36 -0800

    0x06-python-classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+class Square:
+    """Class Square"""
+    def __init__(self, size=0):
+        """[summary]
+        
+        Keyword Arguments:
+            size {int} -- size of the Square (default: {0})
+        """
+        self.__size = size
+        
