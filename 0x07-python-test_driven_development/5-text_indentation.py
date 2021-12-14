490cc1327367b6132e3a552c9fa31fb956d02a74
Phoenix661T <thabangmaloka661@outlook.com>
2021-12-13 22:53:11 -0800

    0x07-python-test_driven_development

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""text_indentation"""
+
+
+def text_indentation(text):
+    """identation of the text given
+    Arguments:
+        text {[str]} -- text/string to be indented
+    Raises:
+        TypeError: text must be a string"
+    """
+    if text is None or not isinstance(text, str) or len(text) < 0:
+        raise TypeError("text must be a string")
+    new_str = "".join([x if x not in ".?:" else x + "\n\n" for x in text])
+    print("\n".join([x.strip() for x in new_str.split("\n")]), end="")
