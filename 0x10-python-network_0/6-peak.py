c282d1985c6a2bb9945b1459578aa3503d48871a
Phoenix661T <thabangmaloka661@outlook.com>
2022-03-17 12:57:49 -0700

    hot fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
+"""Algorithms for list of integers."""
 
 
 def find_peak(list_of_integers):
-        """
-    Finds a peak
-    """
-    if not list_of_integers:
-        return None
-
-    for idx, i in enumerate(list_of_integers):
-        if i == list_of_integers[0] and i > list_of_integers[1]:
-            return i
-
-        if i > list_of_integers[idx - 1] and i > list_of_integers[idx + 1]:
-            return i
-
-    return list_of_integers[len(list_of_integers) - 1]
+    """Finds a peak in a list of unsorted integers."""
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
